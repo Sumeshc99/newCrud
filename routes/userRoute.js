d190d73b4bb63d44eed25f988ce3cb1f23c2729e
sumeshc99 <sumeshchaure99@gmail.com>
2023-11-19 01:22:52 +0530

    crud api

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,12 @@
 import express from "express";
+import { create, deleteUser, fetch, updateUser } from "../controller/userController.js";
 
 const route = express.Router();
-route.get("/fetch", (req, res) => {
-  res.json("hello user");
-});
+
+route.get("/fetch", fetch);
+route.post("/create", create);
+route.put("/update/:id",updateUser)
+route.delete("/delete/:id",deleteUser)
+
 
 export default route;
